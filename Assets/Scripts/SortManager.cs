4576feb102a75953b299c63ffe42a24c16c785ee
PixelVoid <Pixel0Void@gmail.com>
2025-06-04 15:41:27 +0430

    feat: add insertion sort to sort manager

--- Assets/Scripts/SortManager.cs
+++ Assets/Scripts/SortManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 [RequireComponent(typeof(BubbleSort))]
 [RequireComponent(typeof(SelectionSort))]
+[RequireComponent(typeof(InsertionSort))]
 public class SortManager : MonoBehaviour
 {
     private BubbleSort m_BubbleSort;
     private SelectionSort m_SelectionSort;
+    private InsertionSort m_InsertionSort;
 
     private ISortAlgorithm m_CurrentSortAlgorithm;
     [SerializeField] private AlgorithmsEnum m_CurrentAlgorithmEnum;
@@ -17,6 +19,7 @@ public class SortManager : MonoBehaviour
     {
         m_BubbleSort = GetComponent<BubbleSort>();
         m_SelectionSort = GetComponent<SelectionSort>();
+        m_InsertionSort = GetComponent<InsertionSort>();
     }
 
     public void Sort(List<Bar> bars, float delayTime, OrderEnum order, Action onComplete)
@@ -36,6 +39,7 @@ public class SortManager : MonoBehaviour
                 m_CurrentSortAlgorithm = m_SelectionSort;
                 break;
             case AlgorithmsEnum.Insertion:
+                m_CurrentSortAlgorithm = m_InsertionSort;
                 break;
             case AlgorithmsEnum.Shell:
                 break;
