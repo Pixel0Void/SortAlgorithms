7509780faa8811cc799fcd5ac607db90c0d366f6
PixelVoid <Pixel0Void@gmail.com>
2025-06-04 15:32:22 +0430

    feat: add algorithm selection

--- Assets/Scripts/SortManager.cs
+++ Assets/Scripts/SortManager.cs
@@ -10,6 +10,9 @@ public class SortManager : MonoBehaviour
     private BubbleSort m_BubbleSort;
     private SelectionSort m_SelectionSort;
 
+    private ISortAlgorithm m_CurrentSortAlgorithm;
+    [SerializeField] private AlgorithmsEnum m_CurrentAlgorithmEnum;
+
     private void Awake()
     {
         m_BubbleSort = GetComponent<BubbleSort>();
@@ -18,6 +21,28 @@ public class SortManager : MonoBehaviour
 
     public void Sort(List<Bar> bars, float delayTime, OrderEnum order, Action onComplete)
     {
-        m_SelectionSort.Sort(bars, delayTime, order,onComplete);
+        m_CurrentSortAlgorithm.Sort(bars, delayTime, order,onComplete);
+    }
+
+    public void AlgorithmChanged(int value)
+    {
+        m_CurrentAlgorithmEnum = (AlgorithmsEnum)value;
+        switch (m_CurrentAlgorithmEnum)
+        {
+            case AlgorithmsEnum.Bubble:
+                m_CurrentSortAlgorithm = m_BubbleSort;
+                break;
+            case AlgorithmsEnum.Selection:
+                m_CurrentSortAlgorithm = m_SelectionSort;
+                break;
+            case AlgorithmsEnum.Insertion:
+                break;
+            case AlgorithmsEnum.Shell:
+                break;
+            case AlgorithmsEnum.Quick:
+                break;
+            default:
+                goto case AlgorithmsEnum.Bubble;
+        }
     }
 }
