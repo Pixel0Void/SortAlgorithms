effbcad3d6ff37dc3510f7f6bcb0e7113cdb0e48
PixelVoid <Pixel0Void@gmail.com>
2025-06-04 15:00:28 +0430

    feat: create ui for sorting and randomization

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private BarsManager m_BarsManager;
+    [SerializeField] private SortManager m_SortManager;
+
+    [Header("UI Elements")]
+    [SerializeField] private Button m_RandomizeBtn;
+    [SerializeField] private Button m_SortBtn;
+
+    private bool m_IsSorted = false;
+
+    public void OnRandomize()
+    {
+        m_IsSorted = false;
+        m_BarsManager.SetValues();
+    }
+
+    public void OnSort()
+    {
+        if (m_IsSorted)
+            OnRandomize();
+
+        SetButtonsInteractable(false);
+
+        m_SortManager.Sort(m_BarsManager.Bars, 0.01f, () =>
+        {
+            SetButtonsInteractable(true);
+            m_IsSorted = true;
+        });
+    }
+
+    private void SetButtonsInteractable(bool value)
+    {
+        m_RandomizeBtn.interactable = value;
+        m_SortBtn.interactable = value;
+    }
+}
