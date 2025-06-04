b2160842e3eb86ee59947dfba98220e9117838a7
PixelVoid <Pixel0Void@gmail.com>
2025-06-04 14:09:28 +0430

    feat: set bars positions

--- Assets/Scripts/BarsManager.cs
+++ Assets/Scripts/BarsManager.cs
@@ -6,15 +6,20 @@ public class BarsManager : MonoBehaviour
     [SerializeField] private int m_BarsCount;
     public int BarsCount => m_BarsCount;
 
+    [SerializeField] private float m_BarsOffset = 5f;
+
     [SerializeField] private Bar m_BarPrefab;
     [SerializeField] private Transform m_BarsParent;
 
     private List<Bar> m_Bars;
     public List<Bar> Bars => m_Bars;
 
+    private float m_BarWidth = 0f;
+
     private void Start()
     {
         CreateBars();
+        SetPositions();
     }
 
     private void CreateBars()
@@ -23,6 +28,18 @@ public class BarsManager : MonoBehaviour
         for (int i = 0; i < m_BarsCount; i++)
         {
             m_Bars.Add(Instantiate(m_BarPrefab, m_BarsParent));
+        }
+    }
+
+    private void SetPositions()
+    {
+        m_BarWidth = (int)m_Bars[0].GetComponent<RectTransform>().sizeDelta.x;
+        float totalWidth = m_BarWidth * m_BarsCount + (m_BarsCount * m_BarsOffset);
+        float startPositionX = (totalWidth / 2.0f) - totalWidth;
+        foreach (var bar in m_Bars)
+        {
+            bar.GetComponent<RectTransform>().anchoredPosition = new Vector3(startPositionX, 0, 0);
+            startPositionX += m_BarWidth + m_BarsOffset;
         }
     }
 }
