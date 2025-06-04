0fd2705215fc94636232ce81f12c11c67bfefb80
PixelVoid <Pixel0Void@gmail.com>
2025-06-04 15:22:18 +0430

    feat: add selection sort algorithm

---
+++ Assets/Scripts/Algorithms/SelectionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionSort : BaseSort, ISortAlgorithm
+{
+    public void Sort(List<Bar> bars, float delayTime, OrderEnum order, Action onCompelete)
+    {
+        StartCoroutine(Sorting(bars, delayTime, order, onCompelete));
+    }
+
+    private IEnumerator Sorting(List<Bar> bars, float delayTime, OrderEnum order, Action onComplete)
+    {
+        int selectedIndex = 0;
+        int max = bars.Count;
+
+        for (int i = 0; i < max - 1; i++)
+        {
+            selectedIndex = i;
+            for (int j = i + 1; j < max; j++)
+            {
+                if (CompareTool.CompareValues(bars[selectedIndex].Value, bars[j].Value, order))
+                {
+                    selectedIndex = j;
+                }
+            }
+
+            if (selectedIndex != i)
+            {
+                bars[selectedIndex].SetColor(m_ModifiedColor);
+                bars[i].SetColor(m_ModifiedColor);
+
+                yield return new WaitForSeconds(delayTime);
+
+                int temp = bars[selectedIndex].Value;
+                bars[selectedIndex].Value = bars[i].Value;
+                bars[i].Value = temp;
+
+                bars[selectedIndex].ResetColorToOriginal();
+                bars[i].ResetColorToOriginal();
+            }
+        }
+
+        onComplete();
+    }
+}
--- Assets/Scripts/SortManager.cs
+++ Assets/Scripts/SortManager.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BubbleSort))]
+[RequireComponent(typeof(SelectionSort))]
 public class SortManager : MonoBehaviour
 {
     private BubbleSort m_BubbleSort;
+    private SelectionSort m_SelectionSort;
 
     private void Awake()
     {
         m_BubbleSort = GetComponent<BubbleSort>();
+        m_SelectionSort = GetComponent<SelectionSort>();
     }
 
     public void Sort(List<Bar> bars, float delayTime, OrderEnum order, Action onComplete)
     {
-        m_BubbleSort.Sort(bars, delayTime, order,onComplete);
+        m_SelectionSort.Sort(bars, delayTime, order,onComplete);
     }
 }
